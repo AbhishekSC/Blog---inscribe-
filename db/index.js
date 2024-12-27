c2012d76375ec53176520f51f5557d6269ad684d
AbhishekSC <rams31824@gmail.com>
2024-12-27 11:12:30 +0530

    connecting to Mongodb

---
+++ db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+// const uri = "mongodb+srv://rams31824:ram@123@cluster0.k9ud9.mongodb.net/";
+const username = "rams31824";
+const password = "ram@123";
+const cluster = "cluster0.k9ud9.mongodb.net";
+const uri = `mongodb+srv://${username}:${encodeURIComponent(password)}@${cluster}/`;
+
+async function connectToMongoDB() {
+  try {
+    const client = await mongoose.connect(uri);
+
+    console.log("****Successfully connected to MongoDB****");
+    return client;
+  } catch (error) {
+    console.error("****Error connecting to MongoDB:", error);
+  }
+}
+
+export { connectToMongoDB };
