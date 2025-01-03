2c336e82c45110639251e44f79319285efddaf02
Abhishek Singh Chauhan <87475052+AbhishekSC@users.noreply.github.com>
2025-01-03 23:59:37 +0530

    Update index.js
    updated

--- db/index.js
+++ db/index.js
@@ -1,11 +1,5 @@
 import mongoose from "mongoose";
 
-// const uri = "mongodb+srv://rams31824:ram@123@cluster0.k9ud9.mongodb.net/";
-// const username = "rams31824";
-// const password = "ram@123";
-// const cluster = "cluster0.k9ud9.mongodb.net";
-// const uri = `mongodb+srv://${username}:${encodeURIComponent(password)}@${cluster}/`;
-
 async function connectToMongoDB(MONGO_URI) {
   try {
     const client = await mongoose.connect(MONGO_URI);
