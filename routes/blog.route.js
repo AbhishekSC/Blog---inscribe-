f19a54d71df2458a5daeecaaf12adf071a71fb9f
AbhishekSC <rams31824@gmail.com>
2025-01-03 23:57:13 +0530

    controllers for comment and blof view

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,7 +1,11 @@
 // import router
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { handleCreateBlog } from "../controllers/blog.controller.js";
+import {
+  handleCreateBlog,
+  handleCreateComment,
+  handleViewBlog,
+} from "../controllers/blog.controller.js";
 import Blog from "../models/blog.model.js";
 import Comment from "../models/comment.model.js";
 
@@ -16,32 +20,10 @@ router.get("/add-new", (req, res) => {
 });
 
 router.post("/", upload.single("coverImage"), handleCreateBlog);
-router.get("/:id", async (req, res) => {
-  const blogID = req.params.id;
-  const blog = await Blog.findById(blogID).populate("createdBy");
-  const comments = await Comment.find({ blogId: blogID }).populate("createdBy");
-  console.log(blog);
-
-  return res.render("blog", {
-    blog: blog,
-    user: req.user,
-    comments: comments,
-  });
-});
+router.get("/:id", handleViewBlog);
 
 //comment
-router.post("/comment/:blogId", async (req, res) => {
-  const blogId = req.params.blogId;
-  const blog = await Comment.create({
-    content: req.body.content,
-    blogId: blogId,
-    createdBy: req.user._id,
-  });
-
-  console.log(blog);
-
-  return res.redirect(`/blog/${blogId}`);
-});
+router.post("/comment/:blogId", handleCreateComment);
 
 //export router
 export default router;
